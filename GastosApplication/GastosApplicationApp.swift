13571b36f5154fa5ca927345bed3c57a286d2b2b
Sneha Agrawal <snehagrawal98@gmail.com>
2022-01-16 16:36:39 +0530

    Initial Commit

---
+++ GastosApplication/GastosApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GastosApplicationApp.swift
+//  GastosApplication
+//
+//  Created by mnameit on 16/01/22.
+//
+
+import SwiftUI
+
+@main
+struct GastosApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
