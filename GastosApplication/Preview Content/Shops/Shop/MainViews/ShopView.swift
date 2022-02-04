a0f449157fab65773d26328f6112a9d6954a52f7
SaiKasireddi <kasireddisaikumar@gmail.com>
2022-02-04 17:35:32 +0530

    Added Payment view to Account activation section.
    Signed off by: Sai Kumar Kasireddi <kasireddisaikumar@gmail.com>

--- GastosApplication/Preview Content/Shops/Shop/MainViews/ShopView.swift
+++ GastosApplication/Preview Content/Shops/Shop/MainViews/ShopView.swift
@@ -100,3 +100,129 @@ struct ShopView_Previews: PreviewProvider {
         .previewDevice(PreviewDevice(rawValue: "iPhone SE (2nd generation)"))
     }
 }
+
+// Shop Images
+struct ShopImages: View {
+    var body: some View {
+      TabView {
+        ForEach(1..<5) { _ in
+          Image("detailShop")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 0.864 * UIScreen.screenWidth, height: 0.226 * UIScreen.screenHeight)
+            .cornerRadius(10)
+        }
+      }//: TAB
+      .frame(width: 0.864 * UIScreen.screenWidth, height: 0.226 * UIScreen.screenHeight)
+      .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+    }
+}
+
+// Shop Details
+struct ShopDetails: View {
+    var body: some View {
+      HStack {
+        Text("Food & Beverage")
+          .font(.subheadline.weight(.bold))
+          .frame(width: 0.44 * UIScreen.screenWidth, height: 0.046 * UIScreen.screenHeight )
+          .background(Color("textGreen"))
+          .foregroundColor(.white)
+          .cornerRadius(14)
+          .padding(.trailing, 4)
+
+        HStack {
+          Image(systemName: "checkmark.circle.fill")
+            .foregroundColor(Color("textGreen"))
+
+          Text("Delivery")
+        }
+        .foregroundColor(Color("textGreen"))
+        .padding(.horizontal, 4)
+        .font(.caption.weight(.bold))
+
+
+        HStack {
+          Image(systemName: "checkmark.circle.fill")
+            .foregroundColor(Color("textGreen"))
+
+          Text("Pickup")
+        }
+        .foregroundColor(Color("textGreen"))
+        .padding(.horizontal, 4)
+        .font(.caption.weight(.bold))
+      }
+    }
+}
+
+// Contact Us
+struct ContactUs: View {
+  @State var contactName: String
+  @State var address: String
+  var strNumber = "911234567890"
+
+    var body: some View {
+      VStack {
+        HStack {
+          VStack(alignment: .leading) {
+            Text(contactName)
+              .font(.title3.weight(.bold))
+              .foregroundColor(Color("textGreen"))
+
+            Text("here at your service")
+              .font(.subheadline.weight(.medium))
+              .foregroundColor(.gray)
+          }
+          Spacer()
+          Button(action: {
+            // call shop
+            let tel = "tel://"
+            let formattedString = tel + strNumber
+            guard let url = URL(string: formattedString) else { return }
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+          }, label: {
+            VStack {
+              Image(systemName: "phone.fill.arrow.up.right")
+                .scaleEffect(1.5)
+                .padding(8)
+
+              Text("Connect")
+                .font(.caption2.weight(.medium))
+            }
+            .frame(width: 80, height: 38, alignment: .center)
+            .foregroundColor(Color("textGreen"))
+          })
+        }
+        .padding(.horizontal, 24)
+
+        HStack {
+          VStack(alignment: .leading) {
+            Text("Visit us at :")
+              .font(.subheadline.weight(.bold))
+              .foregroundColor(Color("textGreen"))
+              .padding(.vertical, 2)
+
+            Text(address)
+              .font(.subheadline.weight(.regular))
+              .frame(height: 40)
+          }
+          Spacer()
+          Button(action: {
+            // Jump to shop
+          }, label: {
+            VStack {
+              Image(systemName: "paperplane.fill")
+                .scaleEffect(1.5)
+                .padding(8)
+
+              Text("Jump to shop")
+                .font(.caption2.weight(.medium))
+            }
+            .frame(width: 80, height: 38, alignment: .center)
+            .foregroundColor(Color("textGreen"))
+          })
+        }
+        .padding(.vertical, 8)
+        .padding(.horizontal, 24)
+      }
+    }
+}
